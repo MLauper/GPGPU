365a12400f125c6f49097d648d731d66ea3fd822
MLauper <laupm4@bfh.ch>
2017-05-10 12:17:25 +0200

    Implement OpenCL Sample

--- OpenCL-Android/app/src/main/cpp/native-lib.cpp
+++ OpenCL-Android/app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <sstream>
+#include "CL/cl.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -7,5 +9,13 @@ Java_ch_collab_android_opencldemo_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+    cl_uint num_platforms;
+    auto err = ::clGetPlatformIDs(0, NULL, &num_platforms);
+
+    std::ostringstream ss;
+    ss << num_platforms;
+    hello = ss.str();
+
     return env->NewStringUTF(hello.c_str());
 }
