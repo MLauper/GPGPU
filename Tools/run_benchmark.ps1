3cbda02e3ec4c723124995b66e4fadfe16ce8bc9
Marco Lauper <marco.lauper@students.bfh.ch>
2017-06-01 10:08:57 +0200

    Really fix typo

--- Tools/run_benchmark.ps1
+++ Tools/run_benchmark.ps1
@@ -1,3 +1,3 @@
 $desc = Read-Host "Provide system descriptor"
-iex "..\..\GPGPU_Build\CUDA\Release\cuda_benchmark.exe --benchmark_out=${desc}_cuda.csv --benchmark_out_format=t=csv"
-iex "..\..\GPGPU_Build\OpenCL\Release\opencl_benchmark.exe --benchmark_out=${desc}_opencl.csv --benchmark_out_format==csv"
+iex "..\..\GPGPU_Build\CUDA\Release\cuda_benchmark.exe --benchmark_out=${desc}_cuda.csv --benchmark_out_format=csv"
+iex "..\..\GPGPU_Build\OpenCL\Release\opencl_benchmark.exe --benchmark_out=${desc}_opencl.csv --benchmark_out_format=csv"
